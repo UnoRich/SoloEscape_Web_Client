ccc7943ca26690c0d91add810611377704fed7bf
UnoRich <wonyeong91@gmail.com>
2018-04-22 18:03:43 +0900

    Implement Server API call, CROS should be solved
    Signed-off-by: UnoRich <wonyeong91@gmail.com>

--- src/apis/SoloEscape.js
+++ src/apis/SoloEscape.js
@@ -4,21 +4,21 @@ export function getTag(args) {
   return axios.get('http://localhost:4000/tag', {
     ...args,
   })
-  .then( response => { return response } )
+  .then( response => { console.log(response); } )
 }
 
 export function getGoogleUser(args) {
   return axios.get('http://localhost:4000/googleuser', {
     ...args,
   })
-  .then( response => { return response } )
+  .then( response => { console.log(response); } )
 }
 
 export function getLocalUser(args) {
   return axios.get('http://localhost:4000/googleuser', {
     ...args,
   })
-  .then( response => { return response } )
+  .then( response => { console.log(response); } )
 }
 
 export function getProfileById(args) {
